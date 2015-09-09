f65473efcf93dd944cc1727e3cbe9e8285d06e58
Niels Grewe <niels.grewe@halbordnung.de>
2015-09-09 20:13:06 +0200

    Update to track proposed gcc-rs API

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 #[cfg(feature="gnustep_runtime")]
 extern crate gcc;
+#[cfg(feature="gnustep_runtime")]
+use std::path::PathBuf;
+
 
 #[cfg(not(feature="gnustep_runtime"))]
 fn compile() {
@@ -11,8 +14,10 @@ fn compile() {
                       .flag("-fobjc-runtime=gnustep-1.8")
                       .flag("-fno-objc-legacy-dispatch")
                       .file("extern/NSObject.m")
-                      .link(false)
+                      .cargo_metadata(false)
                       .compile("libNSObject.a");
+    let path = ::std::env::var_os("OUT_DIR").map(PathBuf::from).unwrap();
+    println!("cargo:rustc-link-search=native={}", path.display()); 
 }
 fn main() {
     compile();
